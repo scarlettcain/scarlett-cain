3ac42fe8583398af095108a094cf9f35286e5fe2
scarlettcain <scarlett.cain@gmail.com>
2026-06-11 10:05:07 -0400

    Add files via upload

---
+++ gd/gd.js
@@ -0,0 +1,13 @@
+/* ====================================================================================================
+              HAMBURGER MENU
+=======================================================================================================
+ */
+
+const menuToggle = document.getElementById('hamburger');
+const navLinks = document.querySelector('.topnav');
+
+menuToggle.addEventListener('click', () => {
+    console.log("clicked");
+    navLinks.classList.toggle('active');
+})
+
