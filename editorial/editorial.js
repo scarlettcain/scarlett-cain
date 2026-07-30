d1bec74f20d472927012e122565b2fd35bdbfd46
scarlettcain <scarlett.cain@gmail.com>
2026-07-30 10:56:55 -0400

    Add files via upload

---
+++ editorial/editorial.js
@@ -0,0 +1,179 @@
+const horseList = [
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025853/digitalhorses1_ojncle.jpg",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025853/digitalhorses2_gkxzbo.jpg",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025854/digitalhorses3_uv0gcz.jpg",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025854/digitalhorses4_htxf5v.jpg",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025856/digitalHorses5_nqgae5.jpg",
+];
+
+const publiciList = [
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025867/publici1_ksjvkt.jpg",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025869/publici2_bwttbh.jpg",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025869/publici3_kjpx4h.jpg",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025870/publici4_hwnozc.jpg",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025870/publici5_gjfzkq.jpg",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025870/publici6_pn8s0z.jpg",
+];
+
+const ripenedList = [
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025883/ripened1_zic4ff.png",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025876/ripened2_q4tvki.png",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025879/ripened3_kgvfsf.png",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025882/ripened4_khx7uj.png",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025882/ripened5_ikxfn8.png",
+];
+
+
+const hiddenList = [
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025866/hidden1_lqeb1o.jpg",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025865/hidden2_tcj55i.jpg",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025868/hidden3_oqyxtv.jpg",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025869/hidden4_afclvo.jpg",
+];
+
+const reitList = [
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025881/reiteration2_bpsfxi.png",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025882/reiteration4_zdet23.png",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025882/reiteration3_ibjaln.png",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781026285/reiteration1_rrqgdn.png",
+];
+
+
+const glowList = [
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781026281/glow1_gqyiu3.png",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025858/glow2_zxsewk.png",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781026282/glow3_nmrf8a.png",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025870/glow4_frfnlm.png",
+];
+
+const heartlandList = [
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025871/heartland1_y6akrm.png",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025861/heartland2_ebiguc.png",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025869/heartland3_g049z2.png",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781025860/heartland4_mpwscf.png",
+];
+
+const whatIsList = [
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781026292/whatiscover_xi1jer.png",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781026286/whatis1_uql3l7.png",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781026288/whatis2_anl9nl.png",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781026288/whatis3_jgyhxd.png",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781026288/whatis4_ruivhw.png",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781026289/whatis5_tgeqqf.png",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781026290/whatis6_ugxuei.png",
+    "https://res.cloudinary.com/dmr2jbsva/image/upload/q_auto/f_auto/v1781026291/whatis7_jclxtm.png",
+
+];
+
+function createSlideshow(imgId, images) {
+    let current = 0;
+    const img = document.getElementById(imgId);
+
+    img.src = images[current];
+
+    return {
+        next() {
+            current = (current + 1) % images.length;
+            img.src = images[current];
+        },
+
+        prev() {
+            current = (current - 1 + images.length) % images.length;
+            img.src = images[current];
+        }
+    };
+}
+
+const horseShow = createSlideshow("horseimg", horseList);
+const publiciShow = createSlideshow("publicimg", publiciList);
+const ripenedShow = createSlideshow("ripenedimg", ripenedList);
+const hiddenShow = createSlideshow("hiddenimg", hiddenList);
+const reitShow = createSlideshow("reitimg", reitList);
+const glowShow = createSlideshow("glowimg", glowList);
+const heartlandShow = createSlideshow("heartlandimg", heartlandList);
+const whatIsShow = createSlideshow("whatisimg", whatIsList);
+
+
+
+
+
+
+
+
+
+const heartlandP = document.getElementById("heartlandp");
+const ripenedP = document.getElementById("ripenedp");
+const horsesP = document.getElementById("horsesp");
+const reiterationP = document.getElementById("reiterationp");
+const whatIsP = document.getElementById("whatisp");
+const hiddenP = document.getElementById("hiddenp");
+const publiciP = document.getElementById("publicip");
+const glowP = document.getElementById("glowp");
+
+
+const sections = document.querySelectorAll("#heartland, #ripened, #horses, #reiteration, #whatis, #hidden, #publici, #glow");
+
+function hideAll() {
+    heartlandP.classList.remove("active");
+    ripenedP.classList.remove("active");
+    horsesP.classList.remove("active");
+    reiterationP.classList.remove("active");
+    whatIsP.classList.remove("active");
+    hiddenP.classList.remove("active");
+    publiciP.classList.remove("active");
+    glowP.classList.remove("active");
+}
+
+hideAll();
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) return;
+        hideAll();
+        if(entry.target.id ==="heartland") {
+            heartlandP.classList.add("active");
+        }
+        if(entry.target.id ==="ripened") {
+            ripenedP.classList.add("active");
+        }
+        if(entry.target.id ==="horses") {
+            horsesP.classList.add("active");
+        }
+        if(entry.target.id ==="reiteration") {
+            reiterationP.classList.add("active");
+        }
+        if(entry.target.id ==="whatis") {
+            whatIsP.classList.add("active");
+        }
+        if(entry.target.id ==="hidden") {
+            hiddenP.classList.add("active");
+        }
+        if(entry.target.id ==="publici") {
+            publiciP.classList.add("active");
+        }
+        if(entry.target.id ==="glow") {
+            glowP.classList.add("active");
+        }
+
+    });
+}, {
+    threshold: 0,
+    rootMargin: "-35% 0px -65% 0px"
+});
+
+sections.forEach(section => observer.observe(section));
+
+
+
+/* ====================================================================================================
+              HAMBURGER MENU
+=======================================================================================================
+ */
+
+const menuToggle = document.getElementById('hamburger');
+const navLinks = document.querySelector('.topnav');
+
+menuToggle.addEventListener('click', () => {
+    console.log("clicked");
+    navLinks.classList.toggle('active');
+})
